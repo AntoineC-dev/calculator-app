1ebfc3c4da1d28382089cf694fc7bb18f23b4566
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-21 03:04:04 -0400

    Add Max-Age to cookie (7days)

--- src/lib/helpers/cookies.ts
+++ src/lib/helpers/cookies.ts
@@ -1,5 +1,6 @@
 export const getCookieValue = (cookie: string | null, name: string): string | null =>
   cookie?.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || null;
 
+const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
 export const updateCookie = (cookie: string, value: string) =>
-  `${cookie}=${value}; SameSite=Lax; HttpOnly; Path=/; Secure`;
+  `${cookie}=${value}; Max-Age=${ONE_WEEK_IN_SECONDS}; SameSite=Lax; HttpOnly; Path=/; Secure`;
