bfef72466bec68ffebb5e974875938e9d4c33cf9
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-21 02:53:27 -0400

    Update cookie opetions

--- src/lib/helpers/cookies.ts
+++ src/lib/helpers/cookies.ts
@@ -1,4 +1,5 @@
 export const getCookieValue = (cookie: string | null, name: string): string | null =>
   cookie?.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || null;
 
-export const updateCookie = (cookie: string, value: string) => `${cookie}=${value}; SameSite=Strict; HttpOnly; Path=/`;
+export const updateCookie = (cookie: string, value: string) =>
+  `${cookie}=${value}; SameSite=Lax; HttpOnly; Path=/; Secure`;
