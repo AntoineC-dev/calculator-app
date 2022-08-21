bee3bf155958842b2ff35d75da80f0d5e935cffa
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-21 03:09:00 -0400

    Add type check for cookie names

--- src/lib/helpers/cookies.ts
+++ src/lib/helpers/cookies.ts
@@ -1,6 +1,8 @@
+import type { Cookie } from "$lib/types";
+
 export const getCookieValue = (cookie: string | null, name: string): string | null =>
   cookie?.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || null;
 
 const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
-export const updateCookie = (cookie: string, value: string) =>
+export const updateCookie = (cookie: Cookie, value: string) =>
   `${cookie}=${value}; Max-Age=${ONE_WEEK_IN_SECONDS}; SameSite=Lax; HttpOnly; Path=/; Secure`;
