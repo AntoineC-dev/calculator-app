60ea9d51411462fafa6189dca3cd92ee384fb697
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-21 16:12:33 -0400

    Update theme slider size

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -8,16 +8,16 @@
   <h1 class="text-4xl">calc</h1>
   <!-- Theme Switcher -->
   <div class="flex flex-col items-end gap-1">
-    <div class="flex items-center justify-between w-16 px-2">
+    <div class="flex items-center justify-evenly w-20">
       <span class="text-xs leading-none">1</span>
       <span class="text-xs leading-none">2</span>
       <span class="text-xs leading-none">3</span>
     </div>
     <div class="flex items-center gap-6">
       <span class="uppercase text-xs tracking-wider">Theme</span>
-      <div class="w-16 p-1 bg-keypad rounded-2xl transition-colors duration-300">
+      <div class="w-20 p-1 bg-keypad rounded-2xl transition-colors duration-300">
         <div data-theme-slider-inner class="cursor-pointer">
-          <div data-theme-slider-dot class="w-4 aspect-square rounded-full bg-accent transition-all duration-300" />
+          <div data-theme-slider-dot class="w-5 aspect-square rounded-full bg-accent transition-all duration-300" />
         </div>
       </div>
     </div>
